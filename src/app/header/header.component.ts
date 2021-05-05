52e17bb37e6774f327e57dd7d5f88d1d39fba7d1
Marco Mendoza <mmyusuuke@gmail.com>
2021-05-04 23:36:41 -0500

    adding login and recovery session

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
+import {NgbModal, ModalDismissReasons, NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header', 
@@ -15,12 +16,31 @@ export class HeaderComponent {
   // appear initially when the page loads on a small screen!
   public isMenuCollapsed = true;
   images = [700, 533, 807, 124].map((n) => `https://picsum.photos/id/${n}/900/500`);
-
-  constructor(config: NgbCarouselConfig) {
+  closeResult!: string;
+  usersession: string = 'loginSession';
+  constructor(config: NgbCarouselConfig, private modalService: NgbModal) {
     // customize default values of carousels used by this component tree
     config.interval = 10000;
     config.wrap = false;
     config.keyboard = false;
     config.pauseOnHover = false;
+  }
+  
+  open(content: any) {
+    this.modalService.open(content, { size: 'lg' ,ariaLabelledBy: 'modal-basic-title', centered: true }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 }
